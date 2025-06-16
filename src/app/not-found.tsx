b35d0d0f3e44a1f94ea273b8533a3019caba1dbe
Rodrigo Carneiro <carneironline@gmail.com>
2025-06-16 12:52:48 -0300

    feat: add Page not-found

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFoundPage() {
+    return (
+        <div className='p-8 text-center'>
+            <h1 className='text-3xl font-bold'>Página não encontrada</h1>
+            <p className='text-gray-500 mt-4'>A página que você procurou não existe.</p>
+        </div>
+    );
+}
